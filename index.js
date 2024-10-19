a4c8f98154697ba585227c391995faab109ff91e
tina ra <tina.rahimian94@gmail.com>
2024-10-19 14:25:11 -0700

    adding inquiereand shape funcation

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Function to generate SVG content
+function generateSVG(shape, text, textColor, shapeColor) {
+    let shapeSVG = '';
+    if (shape === 'circle') {
+        shapeSVG = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" stroke="black" stoke-width="3"/>`;
+    } else if (shape === 'triangle') {
+        shapeSVG = `<polygon points="150,20 250,200 50,200" fill="${shapeColor}" stroke="black" stoke-width="3"/>`;
+    } else if (shape === 'square') {
+        shapeSVG = `<rect x="70" y="20" width="160" height="160" fill="${shapeColor}"stroke="black" stoke-width="3" />`;
+    } else if (shape === 'ellipse') {
+        shapeSVG = `<ellipse cx="150" cy="100" rx="80" ry="40" fill="${shapeColor}"stroke="black" stoke-width="3"/>`;
+    }
+
+
+    return `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shapeSVG}
+        <text x="150" y="125" font-size="48" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>
+    `;
+}
